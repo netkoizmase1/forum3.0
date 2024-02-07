037204b99adee07928adf311fa6e28398780d6fb
netkoizmase1 <139656313+netkoizmase1@users.noreply.github.com>
2024-02-07 09:37:20 +0100

    Update index.php

--- forum/index.php
+++ forum/index.php
@@ -1,104 +1,80 @@
 using System;
 
+// Produkt
+public interface IVozilo
+{
+    void Vozi();
+}
+
+// KonkretniProduktA
+public class Automobil : IVozilo
+{
+    public void Vozi()
+    {
+        Console.WriteLine("Vožnja automobila");
+    }
+}
+
+// KonkretniProduktB
+public class Bicikl : IVozilo
+{
+    public void Vozi()
+    {
+        Console.WriteLine("Vožnja bicikla");
+    }
+}
+
+// Tvornica
+public interface IProvizija
+{
+    IVozilo IznajmiVozilo();
+}
+
+// KonkretnaTvornicaA
+public class ProvizijaZaAutomobile : IProvizija
+{
+    public IVozilo IznajmiVozilo()
+    {
+        return new Automobil();
+    }
+}
+
+// KonkretnaTvornicaB
+public class ProvizijaZaBicikle : IProvizija
+{
+    public IVozilo IznajmiVozilo()
+    {
+        return new Bicikl();
+    }
+}
+
+// Klijent
 class Program
 {
-    public class Computer
-    {
-        public string CPU { get; set; }
-        public string GPU { get; set; }
-        public int RAM { get; set; }
-
-        public void DisplayInfo()
-        {
-            Console.WriteLine($"Computer with CPU: {CPU}, GPU: {GPU}, RAM: {RAM}GB");
-        }
-    }
-
-    public interface IComputerBuilder
-    {
-        void SetCPU();
-        void SetGPU();
-        void SetRAM();
-        Computer GetComputer();
-    }
-
-    public class GamingComputerBuilder : IComputerBuilder
-    {
-        private Computer computer = new Computer();
-
-        public void SetCPU()
-        {
-            computer.CPU = "High-end gaming CPU";
-        }
-
-        public void SetGPU()
-        {
-            computer.GPU = "Powerful gaming GPU";
-        }
-
-        public void SetRAM()
-        {
-            computer.RAM = 16;
-        }
-
-        public Computer GetComputer()
-        {
-            return computer;
-        }
-    }
-
-    public class OfficeComputerBuilder : IComputerBuilder
-    {
-        private Computer computer = new Computer();
-
-        public void SetCPU()
-        {
-            computer.CPU = "Standard office CPU";
-        }
-
-        public void SetGPU()
-        {
-            computer.GPU = "Basic office GPU";
-        }
-
-        public void SetRAM()
-        {
-            computer.RAM = 8;
-        }
-
-        public Computer GetComputer()
-        {
-            return computer;
-        }
-    }
-
-    public class Director
-    {
-        public Computer BuildComputer(IComputerBuilder builder)
-        {
-            builder.SetCPU();
-            builder.SetGPU();
-            builder.SetRAM();
-            return builder.GetComputer();
-        }
-    }
-
     static void Main()
     {
-        Director director = new Director();
+        Console.WriteLine("Što želite iznajmiti?");
+        Console.WriteLine("1. Automobil");
+        Console.WriteLine("2. Bicikl");
 
-        IComputerBuilder gamingComputerBuilder = new GamingComputerBuilder();
-        Computer gamingComputer = director.BuildComputer(gamingComputerBuilder);
+        string odabir = Console.ReadLine();
 
-        gamingComputer.DisplayInfo();
+        IProvizija provizija;
+        if (odabir == "1")
+        {
+            provizija = new ProvizijaZaAutomobile();
+        }
+        else if (odabir == "2")
+        {
+            provizija = new ProvizijaZaBicikle();
+        }
+        else
+        {
+            Console.WriteLine("Pogrešan odabir. Molimo unesite 1 ili 2.");
+            return;
+        }
 
-        Console.WriteLine();
-
-        IComputerBuilder officeComputerBuilder = new OfficeComputerBuilder();
-        Computer officeComputer = director.BuildComputer(officeComputerBuilder);
-
-        officeComputer.DisplayInfo();
-
-        Console.ReadLine();
+        IVozilo vozilo = provizija.IznajmiVozilo();
+        vozilo.Vozi();
     }
 }
